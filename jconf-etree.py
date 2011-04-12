48be0d2a1ea302da6170105dcdc30a8f51e8582c
Ryan Pavlik <rpavlik@iastate.edu>
2011-04-12 16:30:19 -0500

    set up for command line use.

--- jconf-etree.py
+++ jconf-etree.py
@@ -1,4 +1,5 @@
 import xml.etree.cElementTree as et
+import sys
 
 links = []
 definedNodes = []
@@ -7,25 +8,37 @@ filequeue = []
 
 ns = "{http://www.vrjuggler.org/jccl/xsd/3.0/configuration}"
 
-ignoredElements = ["input_manager", "display_system"]
+ignoredElements = ["input_manager",
+	"display_system",
+	"corba_remote_reconfig",
+	"display_window" # for now, until we recurse into it to find the kb/mouse device, the user for surface projections, and proxies for simulator
+	]
 
 def sanitize(name):
-	return name.replace(" ", "_").replace(".jconf", "").replace("/", "_").replace("\\", "_")
+	return name.replace(" ", "_").replace(".jconf", "").replace("/", "_").replace("\\", "_").replace(".", "_").replace("-", "_")
 
-def addNode(elt):
+def outputNode(name, eltType = "", style = ""):
+	if "proxy" in eltType:
+		pass
+	elif "alias" in eltType:
+		style = style + "style=filled,color=lightgray,"
+	elif "user" in eltType:
+		style = style + "shape=egg,"
+	else:
+		style = style + "shape=box3d,"
+	if eltType == "":
+		label = name
+	else:
+		label = "%s\\n[%s]" % (name, eltType)
+	print('%s [%slabel = "%s"];' % (sanitize(name), style, label))
+def addNode(elt, style = ""):
 	eltName = elt.get("name")
 	eltType = elt.tag.replace(ns, "")
 	definedNodes.append(sanitize(eltName))
-	style = ""
-	if "proxy" in elt.tag:
-		pass
-	elif "alias" in elt.tag:
-		pass
-	else:
-		style = style + "shape=box,"
+
 
 	#print('%s [%slabel = <%s<br/><i>%s</i> >];' % (sanitize(eltName), style, eltName, eltType))
-	print('%s [%slabel = "%s\\n[%s]"];' % (sanitize(eltName), style, eltName, eltType))
+	outputNode(eltName, eltType, style)
 
 def addLink(src, dest, label = None):
 	usedNodes.extend([sanitize(src), sanitize(dest)])
@@ -57,17 +70,18 @@ def processFile(filename):
 	print("subgraph cluster_%s {" % sanitize(filename))
 
 	print('label = "%s";' % filename)
+	print('style = "dotted";')
 
 
 
 	tree = et.parse(filename)
 	root = tree.getroot()
+	included = []
 
 	for firstLevel in list(root):
 		if firstLevel.tag == ns + "include":
-			filequeue.push(firstLevel.text)
-			# todo add an edge indicating the include relationship
-			#links.append("
+			# recurse into included file
+			processFile(firstLevel.text)
 
 		elif firstLevel.tag == ns + "elements":
 
@@ -90,7 +104,7 @@ def processFile(filename):
 					handleProxy(elt, "Digital")
 
 				elif elt.tag == ns + "keyboard_mouse_proxy":
-					handleProxy(elt, "Keyboard/Mouse")
+					handleProxy(elt)
 
 				elif elt.tag == ns + "user":
 					handleUser(elt)
@@ -104,29 +118,30 @@ def processFile(filename):
 
 
 	print("}")
+	return included
 
 def addUndefinedNodes():
 
-	undefined = [ '%s [label="%s",style=dotted];' % (sanitize(x), x)
-		for x in usedNodes if x not in definedNodes ]
+	undefined = [ x for x in usedNodes if x not in definedNodes ]
 	if len(undefined) > 0:
 		print("subgraph cluster_undefined {")
-
 		print('label = "Not defined in these files";')
-		print("\n".join(undefined))
+		print('style = "dotted";')
+		for node in undefined:
+			outputNode(node, style = "style=dashed,")
 		print("}")
 
 def processFiles(files):
 
 	print("digraph {")
-	#print('page="8.5,11"')
-	filequeue = files
-	while len(filequeue) > 0:
-		current = filequeue.pop()
-		processFile(current)
+	print('size="8.5,11"')
+	print('ratio="compress"')
+	for file in files:
+		processFile(file)
 	addUndefinedNodes()
 	print( "\n".join(links))
 	print("}")
 
-processFiles(['IS900TwoWall.jconf'])
+if __name__ == "__main__":
+	processFiles(sys.argv[1:])
 
